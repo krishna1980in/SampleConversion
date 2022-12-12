a08b28b833b09e941772de650e3afe75d19c5f62
Ananthakrishnant <35831@hexaware.com>
2022-12-12 11:26:24 +0530

    DAL changes

--- App_Code/EntityFramework/kartrisConfigData.cs
+++ App_Code/EntityFramework/kartrisConfigData.cs
@@ -1,8 +1,15 @@
 using System.Collections.Generic;
 namespace Kartis
 { 
-    public class kartrisConfigData
+    public class ConfigTblAdptr
     {
+        KartisContext samplecontext;
+        public ConfigTblAdptr()  
+      { 
+        samplecontext=new KartisContext();
+         
+         // This is the constructor method.  
+      }
         
          public string CFG_Name { get; set; }
         
@@ -29,17 +36,52 @@ namespace Kartis
          public string CactuShopName_DEPRECATED { get; set; } 
          public string CFG_Important { get; set; }
         
-        public ICollection<kartrisConfigData> kartrisConfigDatas { get; set; } = new List<kartrisConfigData>();
+        public ICollection<Config> Configs { get; set; } = new List<kartrisConfigData>();
+
+           public IQueryable<kartrisCouponsData> GetConfigByName(string CFG_Name)
+        {
+            SqlParameter pCFG_Name = new SqlParameter("@CFG_Name", CFG_Name);
+            return this.samplecontext.FromSql("EXECUTE spKartrisConfig_GetByName @CFG_Name", CFG_Name);
+        }
+         public IQueryable<kartrisCouponsData> GetConfigDesc(string CFG_Name)
+        {
+            SqlParameter pCFG_Name = new SqlParameter("@CFG_Name", CFG_Name);
+            return this.samplecontext.FromSql("EXECUTE spKartrisConfig_GetDesc @CFG_Name", CFG_Name);
+        }
+          public IQueryable<kartrisCouponsData> GetImportantConfig()
+        {
+            
+            return this.samplecontext.FromSql("EXECUTE spKartrisConfig_GetImportant");
+        }
+         public IQueryable<kartrisCouponsData> SearchConfig(string ConfigKey,bool ImportantConfig)
+        {
+            SqlParameter pConfigKey = new SqlParameter("@ConfigKey", ConfigKey);
+            SqlParameter pImportantConfig = new SqlParameter("@ImportantConfig", ImportantConfig);
+            return this.samplecontext.FromSql("EXECUTE spKartrisConfig_Search @ConfigKey,@ImportantConfig", ConfigKey,ImportantConfig);
+        }
+ 
         
 
     }
-     public class ConfigCache
+     public class ConfigCacheTblAdptr
     {
+          KartisContext samplecontext;
+        public ConfigCacheTblAdptr()  
+      { 
+        samplecontext=new KartisContext();
+         
+         // This is the constructor method.  
+      }
          public string CFG_Name { get; set; }
         
        
         public string CFG_Value { get; set; }
         public ICollection<ConfigCache> ConfigCaches { get; set; } = new List<ConfigCache>();
+        public IQueryable<ConfigCache> GetConfigCacheData()
+        {
+            
+            return this.ConfigCaches.FromSql("EXECUTE spKartrisConfig_GetforCache");
+        }
 
     }
 }
